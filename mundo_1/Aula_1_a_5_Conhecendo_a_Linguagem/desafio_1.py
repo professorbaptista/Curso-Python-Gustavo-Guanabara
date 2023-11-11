8e49baca33829cc33a074ba494330e9ac72564cd
professorbaptista <josbaptista28@yahoo.com.br>
2023-11-11 21:04:05 +0000

    Mudei o placeholder

--- mundo_1/Aula_1_a_5_Conhecendo_a_Linguagem/desafio_1.py
+++ mundo_1/Aula_1_a_5_Conhecendo_a_Linguagem/desafio_1.py
@@ -1,6 +1,6 @@
 # Crie um script Python que leia o nome de uma pessoa e mostre uma mensagem de boas-vindas de acordo com o valor digitado
 
-entrada = input("Digite seu nome: \n")
+entrada = input("Digite o nome em maiúsculas: \n")
 print("Olá, {}! Prazer em conhecer você!".format(entrada))
 
 # Crie um programa que escreva "Olá, mundo" na tela.
